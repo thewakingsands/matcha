eee9adefee5d13f8630e9fc96dc00765c819a3be
Innpaul Zhang <zyp108421@gmail.com>
2021-04-06 18:23:32 +0800

    chore: 5.4

--- Cafe.Matcha/Constant/MatchaOpcode.cs
+++ Cafe.Matcha/Constant/MatchaOpcode.cs
@@ -48,22 +48,22 @@ namespace Cafe.Matcha.Constant
         };
         public static Dictionary<ushort, MatchaOpcode> China = new Dictionary<ushort, MatchaOpcode>
         {
-            { 0x007c, MatchaOpcode.ActorControlSelf },
-            { 0x0144, MatchaOpcode.CEDirector },
-            { 0x0271, MatchaOpcode.CompanyAirshipStatus },
-            { 0x0345, MatchaOpcode.CompanySubmersibleStatus },
-            { 0x02d0, MatchaOpcode.ContentFinderNotifyPop },
-            { 0x02f2, MatchaOpcode.DirectorStart },
-            { 0x01b9, MatchaOpcode.EventPlay },
-            { 0x0316, MatchaOpcode.Examine },
-            { 0x02d2, MatchaOpcode.InitZone },
-            { 0x00a8, MatchaOpcode.InventoryTransaction },
-            { 0x031a, MatchaOpcode.ItemInfo },
-            { 0x0158, MatchaOpcode.MarketBoardItemListing },
-            { 0x0280, MatchaOpcode.MarketBoardItemListingCount },
-            { 0x01f3, MatchaOpcode.MarketBoardItemListingHistory },
-            { 0x01f9, MatchaOpcode.PlayerSetup },
-            { 0x00d1, MatchaOpcode.PlayerSpawn },
+            { 0x030c, MatchaOpcode.ActorControlSelf },
+            { 0x0129, MatchaOpcode.CEDirector },
+            { 0x0179, MatchaOpcode.CompanyAirshipStatus },
+            { 0x0135, MatchaOpcode.CompanySubmersibleStatus },
+            { 0x02b1, MatchaOpcode.ContentFinderNotifyPop },
+            { 0x01b9, MatchaOpcode.DirectorStart },
+            { 0x0374, MatchaOpcode.EventPlay },
+            { 0x012c, MatchaOpcode.Examine },
+            { 0x0245, MatchaOpcode.InitZone },
+            { 0x033b, MatchaOpcode.InventoryTransaction },
+            { 0x01c9, MatchaOpcode.ItemInfo },
+            { 0x03b5, MatchaOpcode.MarketBoardItemListing },
+            { 0x0193, MatchaOpcode.MarketBoardItemListingCount },
+            { 0x012d, MatchaOpcode.MarketBoardItemListingHistory },
+            { 0x010d, MatchaOpcode.PlayerSetup },
+            { 0x030a, MatchaOpcode.PlayerSpawn },
         };
     }
 }
