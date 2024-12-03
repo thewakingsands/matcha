8fad1cd5a79b62b446bb0ccdcdc01be45ab39bae
zhyupe <zyp108421@gmail.com>
2024-12-03 20:37:21 +0800

    fix(MarketBoardHandler): fix not marking MarketBoardItemListing handled

--- Cafe.Matcha/Network/Handler/MarketBoardHandler.cs
+++ Cafe.Matcha/Network/Handler/MarketBoardHandler.cs
@@ -125,6 +125,8 @@ namespace Cafe.Matcha.Network.Handler
                 // Always reset isFirstPageIncoming and queryItemId
                 isFirstPageIncoming = false;
                 queryItemId = 0;
+
+                return true;
             }
 
             return false;
